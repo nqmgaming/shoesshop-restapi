9e1c3f39e332c41a42df6abe62e18da7d1dd6f12
Nguyễn Quang Minh (NQM) <nguyenquangminh570@gmaiil.com>
2024-03-20 11:12:09 +0700

    navigation bottom

--- app/src/main/java/com/nqmgaming/shoesshop/MainActivity.kt
+++ app/src/main/java/com/nqmgaming/shoesshop/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.google.gson.Gson
 import com.nqmgaming.shoesshop.api.ApiService
 import com.nqmgaming.shoesshop.databinding.ActivityMainBinding
@@ -15,6 +17,7 @@ import com.nqmgaming.shoesshop.util.SharedPrefUtils
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -26,12 +29,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.textView.setOnClickListener {
 
-            Toast.makeText(this, "Api", Toast.LENGTH_SHORT).show()
-            val logout = SharedPrefUtils.clear(this)
+        setupBottomNav()
 
+
+    }
+
+    private fun setupBottomNav() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.cartFragment -> {
+                    navController.navigate(R.id.cartFragment)
+                    true
+                }
+                R.id.orderFragment -> {
+                    navController.navigate(R.id.orderFragment)
+                    true
+                }
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
         }
+
 
     }
 
