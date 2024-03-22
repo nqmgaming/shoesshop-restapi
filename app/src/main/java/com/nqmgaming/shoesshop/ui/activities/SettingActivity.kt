dc4ef9306bf482e2fbfbc2d65b100b2b9bde98cf
Nguyễn Quang Minh (NQM) <nguyenquangminh570@gmaiil.com>
2024-03-22 07:18:19 +0700

    logout

---
+++ app/src/main/java/com/nqmgaming/shoesshop/ui/activities/SettingActivity.kt
@@ -0,0 +1,36 @@
+package com.nqmgaming.shoesshop.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nqmgaming.shoesshop.R
+import com.nqmgaming.shoesshop.databinding.ActivitySettingBinding
+import com.nqmgaming.shoesshop.util.SharedPrefUtils
+
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        val view = binding.root
+        enableEdgeToEdge()
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            SharedPrefUtils.clear(this@SettingActivity)
+            //finish all activities
+            val intent = Intent(this@SettingActivity, AuthActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+
+        }
+    }
+}
--- app/src/main/java/com/nqmgaming/shoesshop/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/nqmgaming/shoesshop/ui/fragments/ProfileFragment.kt
@@ -1,22 +1,36 @@
 package com.nqmgaming.shoesshop.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.nqmgaming.shoesshop.R
+import com.nqmgaming.shoesshop.databinding.FragmentProfileBinding
+import com.nqmgaming.shoesshop.ui.activities.SettingActivity
 
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.settingsContainer.setOnClickListener{
+            val intent = Intent(requireContext(), SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
